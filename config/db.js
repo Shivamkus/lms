385e59a016c058c8e43747de27fb201e9f4c153f
shivam kushwah <97247055+Shivamkus@users.noreply.github.com>
2024-01-22 20:34:02 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
- mongoose.connect('mongodb://localhost:27017/design');
-// mongoose.connect('mongodb+srv://kushwahshivam065:cQoRjTw0JGRLsX67@cluster0.60hjjed.mongodb.net/?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true });
+ // mongoose.connect('mongodb://localhost:27017/design');
+mongoose.connect('mongodb+srv://kushwahshivam065:cQoRjTw0JGRLsX67@cluster0.60hjjed.mongodb.net/?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true });
 // mongoose.connect('mongodb+srv://soumyasri2245:Soumya22%4034@cluster0.u2ywt3o.mongodb.net/?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true });
 
  const db = mongoose.connection;
